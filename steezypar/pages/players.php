c994cf7378c1bb29c05b4e3ea949f29a78b78c07
MariePretorius <104560126+MariePretorius@users.noreply.github.com>
2022-06-06 17:44:32 +0200

    Add files via upload

--- steezypar/pages/players.php
+++ steezypar/pages/players.php
@@ -65,9 +65,9 @@
         <li class="nav-item">
           <a class="nav-link text-white " href="../pages/sign-in.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">login</i>
+              <i class="material-icons opacity-10">logout</i>
             </div>
-            <span class="nav-link-text ms-1">Sign In</span>
+            <span class="nav-link-text ms-1">Sign Out</span>
           </a>
         </li>
       </ul>
